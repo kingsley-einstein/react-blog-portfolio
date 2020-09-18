402206d525f9a794929f3e02311bfe4f103ca5cd
Kingsley Victor <javaprodigy56@gmail.com>
2020-09-18 01:06:32 +0100

    New commit

--- public/meta/landing.md
+++ public/meta/landing.md
@@ -1,8 +1,4 @@
 ![Kingsley Victor](../../img/Kingsley_Dev.jpg)
 
 
-##### Full-Stack Software Engineer | Java Citizen | Lover Of JavaScript | Microservices Advocate | GraphQL Tech-naut
-
-```ts
-console.log("Welcome to my blog/portfolio");
-```
+##### Full-Stack Software Engineer | Java Citizen | Lover Of JavaScript | Microservices Advocate | GraphQL Tech-naut &#128526
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,34 @@
 import React from "react";
+import { AppBar, Toolbar, makeStyles } from "@material-ui/core";
 import "./Footer.scss";
 
-const Footer = () => (
-  <div className="footer footer-flex">
-   <div>
-    <i className="fab fa-instagram legible"></i>
-    <i className="fab fa-facebook legible"></i>
-    <i className="fab fa-twitter legible"></i>
-    <i className="fab fa-github legible"></i>
-   </div>
+const useStyles = makeStyles(theme => {
+ return {
+  appBar: {
+   top: "auto",
+   bottom: 0
+  }
+ }
+});
+
+const Footer = () => {
+ const classes = useStyles();
+ return (
+  <div className="footer">
+   <AppBar color="primary" position="fixed" className={classes.appBar}>
+    <Toolbar>
+    <div className="separator"></div>
+     <div className="centered-icons">
+      <i className="fab fa-instagram legible"></i>
+      <i className="fab fa-facebook legible"></i>
+      <i className="fab fa-twitter legible"></i>
+      <i className="fab fa-github legible"></i>
+     </div>
+     <div className="separator"></div>
+    </Toolbar>
+   </AppBar>
   </div>
-);
+ );
+}
 
 export default Footer;
